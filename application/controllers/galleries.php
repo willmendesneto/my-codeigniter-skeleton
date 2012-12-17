3006fc9a50359abd15dbf13825f354fa8322e107
willmendesneto <willmendesneto@gmail.com>
2012-12-17 00:37:43 -0300

    Inserindo Biblioteca: - Image CRUD (http://www.grocerycrud.com/image-crud/)
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

---
+++ application/controllers/galleries.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Galleries extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function index()
+	{
+		try{
+    $crud = new grocery_CRUD();
+
+    $crud->set_theme('twitter-bootstrap');
+    $crud->set_table('offices');
+    $crud->set_subject('Office');
+    $crud->required_fields('city');
+    $crud->columns('city','country','phone');
+    $output = $crud->render();
+
+    $this->_example_output($output);
+		}catch(Exception $e){
+			show_error($e->getMessage().' --- '.$e->getTraceAsString());
+		}
+	}
+
+	public function just_a_test($primary_key , $row)
+	{
+	    return site_url('demo/action/action_photos').'?country='.$row->country;
+	}
+
+	public function _example_output($output = null)
+	{
+		$this->load->view('grocery-crud/example', $output);
+	}
+
+}
---
+++ application/logslog-2012-12-17.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-12-17 01:31:10 --> Config Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Hooks Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Utf8 Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> UTF-8 Support Enabled
+DEBUG - 2012-12-17 01:31:10 --> URI Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Router Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Output Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Security Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Input Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-12-17 01:31:10 --> Language Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Language Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Config Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Loader Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Helper loaded: url_helper
+DEBUG - 2012-12-17 01:31:10 --> Helper loaded: file_helper
+DEBUG - 2012-12-17 01:31:10 --> Database Driver Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Controller Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Config file loaded: D:\projects\my-codeigniter-skeleton\application/config/image_crud.php
+DEBUG - 2012-12-17 01:31:10 --> Image Moo Class Initialized
+DEBUG - 2012-12-17 01:31:10 --> Image Moo Class Initialized
+ERROR - 2012-12-17 01:31:10 --> Query error: Table 'grocery_crud.example_1' doesn't exist - Invalid query: SELECT *
+FROM `example_1`
+DEBUG - 2012-12-17 01:31:10 --> DB Transaction Failure
+DEBUG - 2012-12-17 01:31:10 --> Language file loaded: language/pt-br/db_lang.php
+ERROR - 2012-12-17 01:31:10 --> Severity: Warning  --> include(D:\projects\my-codeigniter-skeleton\application\views/errors/error_db.php) [<a href='function.include'>function.include</a>]: failed to open stream: No such file or directory D:\projects\my-codeigniter-skeleton\system\core\Exceptions.php 149
+ERROR - 2012-12-17 01:31:10 --> Severity: Warning  --> include() [<a href='function.include'>function.include</a>]: Failed opening 'D:\projects\my-codeigniter-skeleton\application\views/errors/error_db.php' for inclusion (include_path='.;C:\Program Files (x86)\Zend\ZendServer\bin\PEAR\pear;C:\Program Files (x86)\Zend\ZendServer\bin\PEAR') D:\projects\my-codeigniter-skeleton\system\core\Exceptions.php 149
