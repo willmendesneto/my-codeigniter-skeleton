8a07cc36096790e24e1db2e526b34028e20e2252
willmendesneto <willmendesneto@gmail.com>
2012-12-18 23:25:05 -0300

    Removendo as chamandas da biblioteca jquery via CDN
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

--- application/views/templates/template.php
+++ application/views/templates/template.php
@@ -14,13 +14,11 @@
     <link rel="stylesheet" href="/assets/twitter-bootstrap/css/bootstrap-responsive.min.css">
     <link rel="stylesheet" href="/assets/twitter-bootstrap/css/style.css">
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="/assets/twitter-bootstrap/js/libs/jquery-1.8.0.min.js"><\/script>')</script>
+    <script src="/assets/twitter-bootstrap/js/libs/jquery-1.8.3.min.js"></script>
 
     <script src="/assets/twitter-bootstrap/js/libs/bootstrap/bootstrap.min.js"></script>
     <script src="/assets/twitter-bootstrap/js/libs/bootstrap/transition.js"></script>
     <script src="/assets/twitter-bootstrap/js/libs/bootstrap/collapse.js"></script>
-    <script src="/assets/twitter-bootstrap/js/script.js"></script>
     <script src="/assets/twitter-bootstrap/js/libs/modernizr-2.6.1.custom.js"></script>
 
     <?php
