4e9125302bab51dcb0ee672a6315f3e73d145458
Will Mendes <willmendesneto@gmail.com>
2014-07-10 14:04:47 -0300

    Update migrate.php
    Now the migrations accept param for version and run all migrations

--- application/controllers/migrate.php
+++ application/controllers/migrate.php
@@ -5,18 +5,32 @@ class Migrate extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper(array('file', 'directory'));
         $this->load->library('migration');
     }
 
-    public function index()
-    {
+    public function all() {
 
-        if ( ! $this->migration->current())
-        {
+        if(!$this->migration->latest()) {
             show_error($this->migration->error_string());
         }
 
-        echo 'Migration ok!';
+        echo 'Migrations up!';
+    }
+
+    public function version($version = '') {
+        if (!is_numeric($version)) {
+            show_error('This method accept only numbers');
+        }
+
+        $migration = $this->migration->version($version);
+        if(!$migration) {
+            show_error($this->migration->error_string());
+        }
+
+        echo 'Migrations "'.$version.'" runs ok!';
     }
 
 }
+/* End of file migrate.php */
+/* Location: ./application/controllers/migrate.php */
